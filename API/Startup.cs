6f230950112197f80f24d60e80864f99fe45f04b
Israel <isra_sousa@hotmail.com>
2019-09-14 19:29:47 +0100

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -39,7 +46,9 @@ namespace API
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
+            
             }
             else
             {
@@ -48,6 +57,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
